b44d7736eb8673c4f0165b44af1ab96319231a2d
rulashype <rsanchezr0403@gmail.com>
2022-08-11 02:42:07 +0000

    Carga de archivo Queries

---
+++ queries.R
@@ -0,0 +1,30 @@
+install.packages("DBI")
+install.packages("RMySQL")
+
+library(DBI)
+library(RMySQL)
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+dbListTables(MyDataBase)
+dbListFields(MyDataBase, 'CountryLanguage')
+DataDB <- dbGetQuery(MyDataBase, "select * from CountryLanguage")
+
+head(DataDB)
+
+
+library(dplyr)
+esp <-  DataDB %>% filter(Language == "Spanish") 
+head(esp)
+
+install.packages("ggplot2")
+library(ggplot2)
+
+ggplot(esp, aes( x = CountryCode, y=Percentage, fill = IsOfficial )) + 
+  geom_bar (stat="identity")  +
+  coord_flip()
+
